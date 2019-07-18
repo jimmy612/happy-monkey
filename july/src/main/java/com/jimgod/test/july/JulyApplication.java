76ec6a1dca2d60b1aca048a4acb457a91155fd2e
jimmy <jimmyhao1992@163.com>
2019-07-18 11:41:05 +0800

    第一次提交

---
+++ july/src/main/java/com/jimgod/test/july/JulyApplication.java
@@ -0,0 +1,13 @@
+package com.jimgod.test.july;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulyApplication.class, args);
+    }
+
+}
